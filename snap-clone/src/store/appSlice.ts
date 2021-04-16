6ffd711bca70359de5d73d305d45e04ea3849476
Bence Papp <bencepapp1101@gmail.com>
2021-04-16 15:11:12 +0200

    redux store created

---
+++ snap-clone/src/store/appSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from './store'
+
+interface CounterState {
+  value: number
+}
+
+const initialState: CounterState = {
+  value: 0
+}
+
+export const appSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: state => {
+      state.value += 1
+    },
+    decrement: state => {
+      state.value -= 1
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload
+    }
+  }
+})
+
+export const { increment, decrement, incrementByAmount } = appSlice.actions
+
+export const selectCount = (state: RootState) => state.app.value
+
+export default appSlice.reducer
