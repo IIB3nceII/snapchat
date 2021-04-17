d875c4fdadd2edb2868ea734e879287a7febb28c
Bence Papp <bencepapp1101@gmail.com>
2021-04-17 15:54:58 +0200

    chat page created

--- snap-clone/src/Chats.tsx
+++ snap-clone/src/Chats.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
-import './Chats.scss';
+import { Avatar } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import "./Chats.scss";
+import SearchIcon from "@material-ui/icons/Search";
+import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
+import { db } from "./firebase";
+
+interface IPosts{
+    id:string;
+    data:any;
+}
 
 function Chats() {
-    return (
-        <div className="chats">
-            
+  const [posts, setPosts] = useState([] as IPosts[]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot: any) =>
+        setPosts(
+          snapshot.docs.map((doc: any) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
+
+  return (
+    <div className="chats">
+      <div className="chats_header">
+        <Avatar className="chats_avatar" />
+        <div className="chats_search">
+          <SearchIcon />
+          <input placeholder="Friends" type="text" />
         </div>
-    )
+        <ChatBubbleIcon className="chats_chatIcon" />
+      </div>
+
+      <div className="chats_posts">
+          {posts.map(({id, data: {profilePic, username, timestamp,imageUrl,read},}))=>(
+
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default Chats;
